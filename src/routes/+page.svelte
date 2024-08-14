93b6375cc47383a9705ef95252dc794576f33347
d3j1x <d3j1x@yahoo.com>
2024-08-14 10:56:15 +0100

    update

---
+++ src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<div dir="ltr" class="justify-center items-center content-center text-center m-2 text-lg font-serif">
+    <span class="justify-center items-center content-center text-center">
+    <span class="font-semibold">Dear users:<br></span>
+    In order to protect the rights and interests of customers,<br>
+    the platform will be maintained and updated from time to time to improve the yield of the GPT version and the stability and security of the platform.<br>
+    Currently, users in some regions/countries are temporarily unable to use it.<br>
+    We understand the feelings of users.<br>
+    The technical department is working hard to repair it.<br>
+    We will notify you after the processing is completed.<br>
+    Thank you for your understanding and support.
+    </span>
+</div>
+
+<br><br>
+
+
+<div dir="ltr" class="justify-center items-center content-center text-center m-2 text-lg font-serif">
+    <span class="justify-center items-center content-center text-center">
+    <span class="font-semibold">Chers utilisateurs:<br></span>
+    Afin de protéger les droits et les intérêts des clients,<br>
+    la plateforme sera maintenue et mise à jour de temps en temps pour améliorer le rendement de la version GPT ainsi que la stabilité et la sécurité de la plateforme.<br>
+    Actuellement, les utilisateurs de certaines régions/pays ne peuvent temporairement pas l'utiliser.<br>
+    Nous comprenons les sentiments des utilisateurs.<br>
+    Le service technique travaille dur pour le réparer.<br>
+    Nous vous informerons une fois le traitement terminé.<br>
+    Merci pour votre compréhension et votre soutien.
+    </span>
+</div>
+
+<br><br>
+
+<div class="justify-center items-center content-center text-center m-2 text-lg font-serif">
+    <span class="justify-center items-center content-center text-center">
+    <span class="font-semibold">عزيزي المستخدمين:<br></span>
+    ومن أجل حماية حقوق ومصالح العملاء،<br>
+    ستتم صيانة النظام الأساسي وتحديثه من وقت لآخر لتحسين إنتاجية إصدار GPT واستقرار النظام الأساسي وأمانه.<br>
+    في الوقت الحالي، يتعذر على المستخدمين في بعض المناطق/البلدان استخدامه مؤقتًا.<br>
+    نحن نتفهم مشاعر المستخدمين.<br>
+    والجهاز الفني يعمل جاهدا على إصلاحه.<br>
+    سنقوم بإخطارك بعد اكتمال المعالجة.<br>
+    شكرا لتفهمك ودعمك.
+    </span>
+</div>
