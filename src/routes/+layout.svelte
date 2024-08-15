8e277d13f729b8aa805eb173e4c3bcef0098fda2
d3j1x <d3j1x@yahoo.com>
2024-08-15 17:55:31 +0100

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,22 @@
 <script>
     import "../app.css";
-    import flogo from "$lib/images/agiogpt.png";
+    import agiologo from "$lib/images/agiogpt2.png";
     import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 
 </script>
 
 
-<Navbar dir="ltr" class="fixed shadow-lg">
+<Navbar dir="ltr" class="fixed shadow-lg bg-black">
     <NavBrand href="/">
-      <img src={flogo} class="me-3 h-12" alt="Flowbite Logo" />
-      <span class="self-center whitespace-nowrap text-xl font-extrabold dark:text-white">AGIO GPT <span class="font-medium">Tunisia</span> </span>
+      <img src={agiologo} class="me-3 h-12 invert-0" alt="AGIOGPT Logo" />
+      <span class="self-center whitespace-nowrap text-xl font-extrabold text-white">AGIO GPT <span class="font-medium">Tunisia</span> </span>
     </NavBrand>
-    <NavHamburger  />
-    <NavUl dir="rtl">
-      <NavLi href="/" class="font-bold">Home</NavLi>
-      <NavLi href="/about" class="font-bold">About</NavLi>
-      <NavLi href="/pricing" class="font-bold">Pricing</NavLi>
-      <NavLi href="/contact" class="font-bold">Contact</NavLi>
+    <NavHamburger menuClass="text-white hover:text-black"  />
+    <NavUl dir="rtl" >
+      <NavLi href="/" class="font-bold md:text-white">Home</NavLi>
+      <NavLi href="/about" class="font-bold md:text-white">About</NavLi>
+      <NavLi href="/pricing" class="font-bold md:text-white">Pricing</NavLi>
+      <NavLi href="/contact" class="font-bold md:text-white">Contact</NavLi>
     </NavUl>
   </Navbar>
 
